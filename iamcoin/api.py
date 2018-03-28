2eb90640ace0957b92ed7f3af233c8224c04aed0
Sanket Patel <sanketplus@gmail.com>
2018-03-29 00:51:02 +0530

    add block API and better imports

--- iamcoin/api.py
+++ iamcoin/api.py
@@ -1,11 +1,18 @@
-import flask
-import blockchain
+from flask import Flask, request, jsonify
+import iamcoin
 import logging
 
 log = logging.getLogger(__name__)
-app= flask.Flask('iamcoin')
+app= Flask('iamcoin')
 
 @app.route('/blockcount')
 def api_get_block_count():
     log.info("Get block count API request.")
-    return flask.jsonify({'count': len(blockchain.blockchain)})
+    return jsonify({'count': len(iamcoin.blockchain)})
+
+@app.route("/addblock",methods=["POST"])
+def api_add_block():
+    if request.method == "POST":
+        block = iamcoin.generate_next_block(request.data)
+        iamcoin.add_block_to_blockchain(block)
+    return jsonify({"response": "success!"})
