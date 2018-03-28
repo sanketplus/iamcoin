a137295c5d6162078bd097998355bc9b32aa2441
Sanket Patel <sanketplus@gmail.com>
2018-03-28 22:58:32 +0530

    added thread for API server

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
+import threading
+import logging
+import signal
+
 from iamcoin.api import app
 
+threads = []
+
+def start_api_server():
+    app.run()
 
 
 if __name__ == "__main__":
-    app.run()
+    logging.info("Start of the world")
+    api_thread = threading.Thread(target=start_api_server, name="API-Thread")
+    api_thread.start()
+
+    threads.append(api_thread)
+
+    for t in threads:
+        t.join()
+    logging.info("Adios:)")
